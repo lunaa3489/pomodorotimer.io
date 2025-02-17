2f1f1e72ae3ab91b2ce13ac95abeb48baa8aff82
LuNa <87761500+lunaa3489@users.noreply.github.com>
2025-02-17 11:11:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Global variables
-let timeLeft = 25 * 60; // seconds
+let timeLeft = 90 * 60; // seconds
 let timerInterval;
 let currentInterval = 'pomodoro';
 let backgroundColor = '#F1F1EF'; // Default background color
@@ -22,19 +22,19 @@ const saveBtn = document.getElementById('save-btn');
 // Event listeners for interval buttons
 pomodoroIntervalBtn.addEventListener('click', () => {
   currentInterval = 'pomodoro';
-  timeLeft = 25 * 60;
+  timeLeft = 90 * 60;
   updateTimeLeftTextContent();
 });
 
 shortBreakIntervalBtn.addEventListener('click', () => {
   currentInterval = 'short-break';
-  timeLeft = 5 * 60;
+  timeLeft = 15 * 60;
   updateTimeLeftTextContent();
 });
 
 longBreakIntervalBtn.addEventListener('click', () => {
   currentInterval = 'long-break';
-  timeLeft = 10 * 60;
+  timeLeft = 30 * 60;
   updateTimeLeftTextContent();
 });
 
@@ -52,11 +52,11 @@ startStopBtn.addEventListener('click', () => {
 resetBtn.addEventListener('click', () => {
   stopTimer();
   if (currentInterval === 'pomodoro') {
-    timeLeft = 25 * 60;
+    timeLeft = 90 * 60;
   } else if (currentInterval === 'short-break') {
-    timeLeft = 5 * 60;
+    timeLeft = 15 * 60;
   } else {
-    timeLeft = 10 * 60;
+    timeLeft = 30 * 60;
   }
   updateTimeLeftTextContent();
   startStopBtn.textContent = 'Start';
@@ -96,15 +96,15 @@ function startTimer() {
     if (timeLeft === 0) {
       clearInterval(timerInterval);
       if (currentInterval === 'pomodoro') {
-        timeLeft = 5 * 60;
+        timeLeft = 15 * 60;
         currentInterval = 'short-break';
         startTimer();
       } else if (currentInterval === 'short-break') {
-        timeLeft = 10 * 60;
+        timeLeft = 30 * 60;
         currentInterval = 'long-break';
         startTimer();
       } else {
-        timeLeft = 25 * 60;
+        timeLeft = 90 * 60;
         currentInterval = 'pomodoro';
       }
     }
